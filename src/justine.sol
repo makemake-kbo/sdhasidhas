2092c3a594c870c51cc750ba9b9a590f1a4156fb
Vukasin Gostovic <vukasin@gostovic.me>
2023-07-21 22:27:43 +0200

    add some basic things to justine

--- src/justine.sol
+++ src/justine.sol
@@ -44,6 +44,16 @@ contract Justine is BaseHook {
         override
         returns (bytes4)
     {
+        // Get how much eth we're depositing
+        uint ethAmount;   
+        if (isAmount0Eth) {
+            ethAmount = amount0;
+        } else {
+            ethAmount = amount1;
+        }
+
+        
+
         return BaseHook.beforeSwap.selector;
     }
 
@@ -52,6 +62,7 @@ contract Justine is BaseHook {
         IPoolManager.PoolKey calldata key,
         ModifyPositionParams.ModifyParams calldata params
     ) external override returns (bytes4) {
+
         return BaseHook.beforeSwap.selector;
     }
 }
